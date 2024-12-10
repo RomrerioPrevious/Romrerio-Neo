ce67584bdee9d72b2a99429e7ef433e69657db1e
RomrerioPrevious <ggJohn12345@yandex.ru>
2024-12-10 18:43:23 +0300

    New tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -48,4 +48,18 @@ require("neo-tree").setup({
           position = "left",
           width = 35,
     },
+    filesystem = {
+         filtered_items = {
+            visible = true,
+            show_hidden_count = true,
+            hide_dotfiles = false,
+            hide_gitignored = true,
+            hide_by_name = {
+                -- '.git',
+                -- '.DS_Store',
+                -- 'thumbs.db',
+            },
+            never_show = {},
+        },
+    },
 })
